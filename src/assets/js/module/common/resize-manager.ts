6b339972d7a5857f4dbdd9c8c6de68df97c6dcfb
borntoolate <ctrncl.e@gmail.com>
2022-02-28 16:28:01 +0900

    Fix: JSウィンドウリサイズマネージャーのremoveメソッド修正

--- src/assets/js/module/common/resize-manager.ts
+++ src/assets/js/module/common/resize-manager.ts
@@ -2,17 +2,17 @@
 type myFunction = () => void;
 
 export class ResizeManager {
-  windowWidth: number;
-  windowHeight: number;
-  functions: myFunction[];
-  length: number;
+  winWidth: number;
+  winHeight: number;
+  fnArray: myFunction[];
+  fnArrayLength: number;
   fps: number;
   isRunning: boolean;
   constructor() {
-    this.windowWidth = 0;
-    this.windowHeight = 0;
-    this.functions = [];
-    this.length = 0;
+    this.winWidth = 0;
+    this.winHeight = 0;
+    this.fnArray = [];
+    this.fnArrayLength = 0;
     this.fps = 60;
     this.isRunning = false;
   }
@@ -39,26 +39,29 @@ export class ResizeManager {
     );
   }
 
-  add(targetFunction: myFunction): void {
-    if (targetFunction && typeof targetFunction === 'function') {
-      this.functions.push(targetFunction);
-      this.length = this.functions.length;
+  add(targetFn: myFunction): void {
+    if (targetFn && typeof targetFn === 'function') {
+      this.fnArray.push(targetFn);
+      this.fnArrayLength = this.fnArray.length;
     }
   }
 
-  // remove(targetFunction: myFunction) void {
-  //   this.functions.splice(targetFunction, 1);
-  //   this.length = this.functions.length;
-  // }
+  remove(targetFn: myFunction): void {
+    if (targetFn && typeof targetFn === 'function') {
+      const fnOrder = this.fnArray.indexOf(targetFn);
+      this.fnArray.splice(fnOrder, 1);
+      this.fnArrayLength = this.fnArray.length;
+    }
+  }
 
   update(): void {
-    this.windowWidth = window.innerWidth;
-    this.windowHeight = window.innerWidth;
+    this.winWidth = window.innerWidth;
+    this.winHeight = window.innerHeight;
 
-    for (let i = 0; i < this.length; i++) {
-      const thisFunction = this.functions[i];
-      if (thisFunction && typeof thisFunction === 'function') {
-        thisFunction();
+    for (let i = 0; i < this.fnArrayLength; i++) {
+      const handledFn = this.fnArray[i];
+      if (handledFn && typeof handledFn === 'function') {
+        handledFn();
       }
     }
 
